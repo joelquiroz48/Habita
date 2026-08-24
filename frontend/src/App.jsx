a31c99125e948bed259d6128d30639c9dcebbb80
Nizusao <nizusao@gmail.com>
2026-08-23 23:08:34 -0300

    integre el supabase in your face

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { supabase } from './supabase'
 
 function App() {
+  
   const [count, setCount] = useState(0)
 
   return (
